3560c78bfde360cba370e3ba2094a4357b6c8f95
Danilo Batista Queiroz <danilobatistaqueiroz@gmail.com>
2018-09-19 22:34:49 -0300

    sqlite working

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('laravel.sqlite')),
+            'database' => env('DB_DATABASE/..', database_path('laravel.sqlite')),
             'prefix' => '',
         ],
 
